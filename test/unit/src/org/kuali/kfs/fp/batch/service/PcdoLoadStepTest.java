179da3405b76c16e1f4d0981f5d1fdddc17d8a00
Shawn Bower <shawn.bower@gmail.com>
2014-06-10 05:19:13 -0400

    adding some unit test

--- test/unit/src/org/kuali/kfs/fp/batch/service/PcdoLoadStepTest.java
+++ test/unit/src/org/kuali/kfs/fp/batch/service/PcdoLoadStepTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,16 @@
 package org.kuali.kfs.fp.batch.service;
 
 import java.io.File;
+import java.util.Collection;
 
+import org.apache.commons.io.FileUtils;
 import org.kuali.kfs.fp.batch.ProcurementCardInputFileType;
+import org.kuali.kfs.fp.batch.ProcurementCardLoadStep;
+import org.kuali.kfs.fp.businessobject.ProcurementCardTransaction;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.krad.service.BusinessObjectService;
 
 /**
  * Tests the PcdoLoadStep. DEPENDENCIES: Procurement card xml file transaction1.xml must be in /opt/kuali/dev/staging/PCDO/ this
@@ -29,6 +34,8 @@ import org.kuali.kfs.sys.context.SpringContext;
 @ConfigureContext
 public class PcdoLoadStepTest extends KualiTestBase {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PcdoLoadStepTest.class);
+    private static final String DATA_FILE_PATH = "test/unit/src/org/kuali/kfs/fp/batch/fixture/transactionsUnit.xml";
+    private String batchDirectory;
 
     public PcdoLoadStepTest() {
         super();
@@ -36,14 +43,25 @@ public class PcdoLoadStepTest extends KualiTestBase {
 
     /**
      * Creats .done file for test input file.
-     * 
+     *
      * @see junit.framework.TestCase#setUp()
      */
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
-        String doneFileName = SpringContext.getBean(ProcurementCardInputFileType.class).getDirectoryPath() + "/transactions1.done";
+        //make sure we have a batch directory
+        batchDirectory =  SpringContext.getBean(ProcurementCardInputFileType.class).getDirectoryPath();
+        File batchDirectoryFile = new File(batchDirectory);
+        batchDirectoryFile.mkdir();
+
+        //copy the data file into place
+        File dataFileSrc = new File(DATA_FILE_PATH);
+        File dataFileDest = new File(batchDirectory + "/transactionsUnit.xml");
+        FileUtils.copyFile(dataFileSrc, dataFileDest);
+
+        //create .done file
+        String doneFileName = batchDirectory + "/transactionsUnit.done";
         File doneFile = new File(doneFileName);
         if (!doneFile.exists()) {
             LOG.info("Creating done file: " + doneFile.getAbsolutePath());
@@ -52,19 +70,38 @@ public class PcdoLoadStepTest extends KualiTestBase {
     }
 
     /**
+     * Removes the directory for the batch files to go in
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    public void tearDown() throws Exception {
+        /*File batchDirectoryFile = new File(batchDirectory);
+        for (File f : batchDirectoryFile.listFiles()) {
+            f.delete();
+        }
+        batchDirectoryFile.delete();*/
+    }
+
+    /**
      * Tests the whole step completes successfully.
      */
     public void testAll() throws Exception {
         assertTrue("hold until figure out staging dir!", true);
-        //        
-        // PcdoLoadStep pcdoLoadStep = SpringContext.getBean(PcdoLoadStep.class);
-        // boolean goodExit = pcdoLoadStep.execute();
-        //        
-        // assertTrue("pcdo load step did not exit with pass", goodExit);
-        //        
-        // Collection loadedTransactions =
-        // SpringContext.getBean(BusinessObjectService.class).findAll(ProcurementCardTransaction.class);
-        // assertNotNull("no transactions loaded ", loadedTransactions);
-        // assertEquals("incorrect number of transactions loaded ",10,loadedTransactions.size());
+
+         ProcurementCardLoadStep pcdoLoadStep = SpringContext.getBean(ProcurementCardLoadStep.class);
+         ProcurementCardInputFileType pcdoFileType = SpringContext.getBean(ProcurementCardInputFileType.class);
+         pcdoFileType.setSchemaLocation("http://testdrive.kfs.kuali.org/kfs-ptd/static/xsd/fp/procurementCard.xsd");
+         //pcdoFileType.setSchemaLocation("file:///" + System.getProperty("user.dir")+ "/build/project/xsd/fp/procurementCard.xsd");
+
+         pcdoLoadStep.setBatchInputFileType(pcdoFileType);
+
+         boolean goodExit = pcdoLoadStep.execute("test", null);
+
+         assertTrue("pcdo load step did not exit with pass", goodExit);
+
+         Collection<ProcurementCardTransaction> loadedTransactions =
+                 SpringContext.getBean(BusinessObjectService.class).findAll(ProcurementCardTransaction.class);
+         assertNotNull("no transactions loaded ", loadedTransactions);
+         assertEquals("incorrect number of transactions loaded ",20,loadedTransactions.size());
     }
 }
--- test/unit/src/org/kuali/kfs/fp/batch/service/ProcurementCardDocumentServiceTest.java
+++ test/unit/src/org/kuali/kfs/fp/batch/service/ProcurementCardDocumentServiceTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,15 +17,40 @@ package org.kuali.kfs.fp.batch.service;
 
 import static org.kuali.kfs.sys.fixture.UserNameFixture.kfs;
 
+import org.kuali.kfs.fp.businessobject.ProcurementCardTransaction;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.krad.service.BusinessObjectService;
 
 /**
  * This class tests the services used to create ProcurementCard documents.
  */
 @ConfigureContext(session = kfs)
 public class ProcurementCardDocumentServiceTest extends KualiTestBase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        ProcurementCardTransaction transaction = new ProcurementCardTransaction();
+        transaction.setTransactionCreditCardNumber("999911112222333");
+        transaction.setFinancialDocumentTotalAmount("11.55");
+        transaction.setTransactionDebitCreditCode("D");
+        transaction.setChartOfAccountsCode("BL");
+        transaction.setAccountNumber("4631654");
+        transaction.setFinancialObjectCode("4190");
+        transaction.setTransactionCycleStartDate("2006-02-20");
+        transaction.setTransactionCycleEndDate("2006-02-20");
+        transaction.setCardHolderName("Molecular Structure Center");
+        transaction.setTransactionDate(java.sql.Date.valueOf("2006-02-17"));
+        transaction.setTransactionReferenceNumber("554173460487304813890");
+        transaction.setTransactionMerchantCategoryCode("7622");
+        transaction.setTransactionPostingDate(java.sql.Date.valueOf("2006-02-17"));
+        transaction.setVendorName("STANSIFER RADIO CO");
+        SpringContext.getBean(BusinessObjectService.class).save(transaction);
+
+    }
 
     public void testCreatePCardDocuments() throws Exception {
         boolean documentsCreated = SpringContext.getBean(ProcurementCardCreateDocumentService.class).createProcurementCardDocuments();
